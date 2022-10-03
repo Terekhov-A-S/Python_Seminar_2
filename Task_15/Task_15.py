f91f99a615f308d6dac649c937b81c2b96692948
KoT <Kotto@student.su>
2022-10-03 19:28:48 +0400

    added task 15

---
+++ Task_15/Task_15.py
@@ -0,0 +1,14 @@
+# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N
+
+def Check_factorial (num):
+    count = 1
+    for i in range(1, num + 1):
+        count *= i
+    return count
+j = int (input ('Введите число: '))
+print (f'Полученный набор призведений от 1 до {j} : ', end = '')
+for i in range(1, j + 1):
+    if i == j: 
+        print (f'{Check_factorial(i)}')
+    else:
+        print (f'{Check_factorial(i)}', end = ', ')
